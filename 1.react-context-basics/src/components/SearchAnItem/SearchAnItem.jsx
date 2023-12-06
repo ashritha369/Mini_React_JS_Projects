a2d7c7a2452675ff47ef430555a4fb3e5d0c1993
unknown <ashrithagowda666@gmail.com>
2023-12-06 19:07:28 +0530

    worked on the component search an item

--- 1.react-context-basics/src/components/SearchAnItem/SearchAnItem.jsx
+++ 1.react-context-basics/src/components/SearchAnItem/SearchAnItem.jsx
@@ -1,6 +1,43 @@
-const SearchAnItem=()=>{
-    return(
-        <div className="searchAnItem-container">SearchAnItem</div>
-    )
-}
+import {  useState } from "react";
+import jsondata from "../../jsondata.json";
+
+const SearchAnItem = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [searchedItem, setSearchedItem] = useState("");
+  const arrayOfTitles = [];
+  // to get arrayOfTitles
+  jsondata.forEach((item) => arrayOfTitles.push(item.title));
+  //to get input values and setting it to varaible
+  const onEnterValueHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // inputValue needs to be searched in arrayOfTitles
+  const handleSearch = () => {
+    if (arrayOfTitles.includes(inputValue)) {
+      setSearchedItem(inputValue);
+    } else {
+      setSearchedItem("Search Item Not Found");
+    }
+  };
+
+  return (
+    <div className="searchAnItem-container">
+      <input
+        className="input-bar"
+        value={inputValue}
+        onChange={onEnterValueHandler}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <div className="searchedItem">
+        Searched Item is : <h3>{searchedItem}</h3>
+      </div>
+      <div className="list-of-items">
+        {jsondata.map((item) => {
+          return <div key={item.title}>{item.title}</div>;
+        })}
+      </div>
+    </div>
+  );
+};
 export default SearchAnItem;
