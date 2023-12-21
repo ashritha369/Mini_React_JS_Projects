c9ff4a722a4f9c2ae6737a839faf60be317c239d
unknown <ashrithagowda666@gmail.com>
2023-12-21 14:21:38 +0530

    added readme for bottom-to-up

--- 1.react-context-basics/src/components/BottomToTop/BottomToTop.jsx
+++
@@ -1,6 +0,0 @@
-const BottomToTop=()=>{
-    return(
-        <div className="bottomToTop-container">BottomToTop</div>
-    )
-}
-export default BottomToTop;
---
+++ 1.react-context-basics/src/components/BottomToTop/BottomToTop.md
@@ -0,0 +1,4 @@
+![Alt text](Imgs/image1.png)
+![Alt text](Imgs/image2.png)
+![Alt text](Imgs/image3.png)
+![Alt text](Imgs/image4.png)
